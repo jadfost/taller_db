0ab67439450cf19d265d452d1e0951ffbe404bb7
ivangonzalez02 <ivafelgonzalez@uniboyaca.edu.co>
2022-10-11 18:00:59 -0500

    22

---
+++ app/src/androidTest/java/com/airthcompany/gradecalculator/ApplicationTestCase.java
@@ -0,0 +1,8 @@
+package com.airthcompany.gradecalculator;
+
+import android.app.Application;
+
+public class ApplicationTestCase<T> {
+    public ApplicationTestCase(Class<Application> applicationClass) {
+    }
+}
