04ce36cfeced7de2c2de8f4839b516b209582b66
ivangonzalez02 <ivafelgonzalez@uniboyaca.edu.co>
2022-10-11 17:34:38 -0500

    2

---
+++ app/src/main/java/com/airthcompany/gradecalculator/RowData.java
@@ -0,0 +1,37 @@
+package com.airthcompany.gradecalculator;
+
+import android.widget.Button;
+
+public class RowData {
+
+    private int id;
+    private String name;
+    private int score;
+    private int weight;
+    private Button button;
+
+    public RowData(int id, String name, int score, int weight){
+        this.id = id;
+        this.name = name;
+        this.score = score;
+        this.weight = weight;
+    }
+
+    // captadores
+
+    public int getId(){  return id;    }
+    public String getName(){  return name;  }
+    public int getScore(){ return score; }
+    public int getWeight(){ return weight; }
+
+
+    // Setters/Cambiadores
+
+    public void setId(int id){ this.id = id; }
+    public void setName(String name){ this.name = name; }
+    public void setScore(int score){ this.score = score; }
+    public void setWeight(int weight){ this.weight = weight; }
+    public void setButton(Button button){ this.button = button; }
+
+
+}
