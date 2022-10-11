5996ffed825764a86b8b223a1eb0c123cc43d9f5
nirttson <stevenorduz@hotmail.com>
2022-10-11 18:37:25 -0500

    26

---
+++ app/src/main/java/com/airthcompany/gradecalculator/activity_home.java
@@ -0,0 +1,13 @@
+package com.airthcompany.gradecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
